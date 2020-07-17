5dc59dcb1b27fdbe956cc18f173378d1576bdd4e
shraddhaduseja24 <shraddhaduseja24@gmail.com>
2020-07-18 03:22:51 +0530

    providing inline style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
-let defaultTextColor = '#ffff';
 let defaultStyle = {
-  color: defaultTextColor
+  color: '#ffff'
 }
 class Aggregate extends Component {
   render () {
@@ -47,7 +45,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style = {{...defaultStyle, 'fontSize': '54px'}}>Title</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
